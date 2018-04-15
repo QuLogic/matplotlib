75ea83d006aed1c7423bf1a0a23b0efb445dded0
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2018-04-15 00:21:29 -0400

    Use GtkShortcutsWindow for Help tool.

--- lib/matplotlib/backends/backend_gtk3.py
+++ lib/matplotlib/backends/backend_gtk3.py
@@ -847,13 +847,73 @@ class ConfigureSubplotsGTK3(backend_tools.ConfigureSubplotsBase, Gtk.Window):
 
 
 class HelpGTK3(backend_tools.ToolHelpBase):
-    def trigger(self, *args):
+    def _normalize_shortcut(self, key):
+        """
+        Convert Matplotlib key presses to GTK+ accelerator identifiers.
+
+        Related to `FigureCanvasGTK3._get_key`.
+        """
+        special = {
+            'backspace': 'BackSpace',
+            'pagedown': 'Page_Down',
+            'pageup': 'Page_Up',
+            'scroll_lock': 'Scroll_Lock',
+        }
+
+        parts = key.split('+')
+        mods = ['<' + mod + '>' for mod in parts[:-1]]
+        key = parts[-1]
+
+        if key in special:
+            key = special[key]
+        elif len(key) > 1:
+            key = key.capitalize()
+        elif key.isupper():
+            mods += ['<shift>']
+
+        return ''.join(mods) + key
+
+    def _show_shortcuts_window(self):
+        section = Gtk.ShortcutsSection()
+        group = Gtk.ShortcutsGroup()
+        section.add(group)
+
+        for name, tool in sorted(self.toolmanager.tools.items()):
+            if not tool.description:
+                continue
+
+            shortcut = Gtk.ShortcutsShortcut(
+                accelerator=' '.join(
+                    self._normalize_shortcut(key)
+                    for key in self.toolmanager.get_tool_keymap(name)
+                    # Will never be sent:
+                    if 'cmd+' not in key),
+                title=tool.name,
+                subtitle=tool.description)
+            group.add(shortcut)
+
+        window = Gtk.ShortcutsWindow(
+            title='Help',
+            modal=True,
+            transient_for=self._figure.canvas.get_toplevel())
+        section.show()  # Must be done explicitly before add!
+        window.add(section)
+
+        window.show_all()
+
+    def _show_shortcuts_dialog(self):
         dialog = Gtk.MessageDialog(
             self._figure.canvas.get_toplevel(),
             0, Gtk.MessageType.INFO, Gtk.ButtonsType.OK, self._get_help_text(),
             title="Help")
         dialog.run()
         dialog.destroy()
+
+    def trigger(self, *args):
+        if Gtk.check_version(3, 20, 0) is None:
+            self._show_shortcuts_window()
+        else:
+            self._show_shortcuts_dialog()
 
 
 # Define the file to use as the GTk icon
