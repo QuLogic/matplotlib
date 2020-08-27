764ab1d7363a89df5a821ff8ef9813832b3e71bf
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2020-08-26 20:29:47 -0400

    Deprecate passing incorrect types to `Axes.add_*`.

--- lib/matplotlib/axes/_base.py
+++ lib/matplotlib/axes/_base.py
@@ -2085,6 +2085,20 @@ class _AxesBase(martist.Artist):
                                   mlines.Line2D, mpatches.Patch))
                    for a in self._children)
 
+    def _deprecate_noninstance(self, _name, _types, **kwargs):
+        """
+        For each *key, value* pair in *kwargs*, check that *value* is an
+        instance of one of *_types*; if not, raise an appropriate deprecation.
+        """
+        for key, value in kwargs.items():
+            if not isinstance(value, _types):
+                cbook.warn_deprecated(
+                    '3.4', name=_name,
+                    message=f'Passing argument *{key}* of unexpected type '
+                    f'{type(value).__qualname__} to %(name)s which only '
+                    f'accepts {_types} is deprecated since %(since)s and will '
+                    'become an error %(removal)s.')
+
     def add_artist(self, a):
         """
         Add an `~.Artist` to the Axes; return the artist.
@@ -2128,6 +2142,8 @@ class _AxesBase(martist.Artist):
         """
         Add a `~.Collection` to the Axes; return the collection.
         """
+        self._deprecate_noninstance('add_collection', mcoll.Collection,
+                                    collection=collection)
         label = collection.get_label()
         if not label:
             collection.set_label(f'_child{len(self._children)}')
@@ -2160,6 +2176,7 @@ class _AxesBase(martist.Artist):
         """
         Add an `~.AxesImage` to the Axes; return the image.
         """
+        self._deprecate_noninstance('add_image', mimage.AxesImage, image=image)
         self._set_artist_props(image)
         if not image.get_label():
             image.set_label(f'_child{len(self._children)}')
@@ -2176,6 +2193,7 @@ class _AxesBase(martist.Artist):
         """
         Add a `.Line2D` to the Axes; return the line.
         """
+        self._deprecate_noninstance('add_line', mlines.Line2D, line=line)
         self._set_artist_props(line)
         if line.get_clip_path() is None:
             line.set_clip_path(self.patch)
@@ -2192,6 +2210,7 @@ class _AxesBase(martist.Artist):
         """
         Add a `~.Text` to the Axes; return the text.
         """
+        self._deprecate_noninstance('_add_text', mtext.Text, txt=txt)
         self._set_artist_props(txt)
         self._children.append(txt)
         txt._remove_method = self._children.remove
@@ -2246,6 +2265,7 @@ class _AxesBase(martist.Artist):
         """
         Add a `~.Patch` to the Axes; return the patch.
         """
+        self._deprecate_noninstance('add_patch', mpatches.Patch, p=p)
         self._set_artist_props(p)
         if p.get_clip_path() is None:
             p.set_clip_path(self.patch)
@@ -2286,6 +2306,7 @@ class _AxesBase(martist.Artist):
         """
         Add a `~.Table` to the Axes; return the table.
         """
+        self._deprecate_noninstance('add_table', mtable.Table, tab=tab)
         self._set_artist_props(tab)
         self._children.append(tab)
         tab.set_clip_path(self.patch)
