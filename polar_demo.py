66faa2036901259395118ce18f0dd9578211ed7d
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2024-09-16 19:33:45 -0400

    Simplify polar demo using constrained layout

--- polar_demo.py
+++ polar_demo.py
@@ -7,9 +7,10 @@ import matplotlib.widgets as mwidgets
 x = np.arange(0, 7, 0.001)
 y = np.arange(-2.0, 5.0, 0.001)
 
-fig = plt.figure()
+fig = plt.figure(layout='constrained')
+top, bottom = fig.subfigures(2, 1)
 
-ax = fig.add_axes((0, 0.36, 1, 0.57), polar=True, facecolor='red')
+ax = top.subplots(subplot_kw={'polar': True, 'facecolor': 'red'})
 ax.plot(x, y)
 ax.set_title('Title')
 
@@ -27,61 +28,27 @@ ax.xaxis.set_ticks_position('both')
 ax.yaxis.set_ticks_position('both')
 
 
-def update_thetamin(val):
-    ax.set_thetamin(val)
-    fig.canvas.draw()
-
-
-def update_thetamax(val):
-    ax.set_thetamax(val)
-    fig.canvas.draw()
-
-
-def update_rorigin(val):
-    ax.set_rorigin(val)
-    fig.canvas.draw()
-
-
-def update_rmin(val):
-    ax.set_rmin(val)
-    fig.canvas.draw()
-
-
-def update_rmax(val):
-    ax.set_rmax(val)
-    fig.canvas.draw()
-
-
 def update_dir(val):
     if val == 'CCW':
         ax.set_theta_direction('counterclockwise')
     else:
         ax.set_theta_direction('clockwise')
-    fig.canvas.draw()
 
 
-def update_offset(val):
-    ax.set_theta_zero_location(val)
-    fig.canvas.draw()
+radios, sliders = bottom.subfigures(2, 1)
 
-
-axtmin = fig.add_axes((0.15, 0.01, 0.75, 0.05))
-axtmax = fig.add_axes((0.15, 0.06, 0.75, 0.05))
-axrorigin = fig.add_axes((0.15, 0.11, 0.75, 0.05))
-axrmin = fig.add_axes((0.15, 0.16, 0.75, 0.05))
-axrmax = fig.add_axes((0.15, 0.21, 0.75, 0.05))
-axdir = fig.add_axes((0.1, 0.26, 0.5, 0.1), facecolor='none')
+axdir, axoffs = radios.subplots(1, 2)
 axdir.axis('off')
-axoffs = fig.add_axes((0.5, 0.26, 0.5, 0.1), facecolor='none')
 axoffs.axis('off')
+axrmax, axrmin, axrorigin, axtmax, axtmin = sliders.subplots(5, 1)
 
 stmin = mwidgets.Slider(axtmin, 'thetamin', 0, 360, valinit=ax.get_thetamin(),
                         closedmax=False)
 stmax = mwidgets.Slider(axtmax, 'thetamax', 0, 360, valinit=ax.get_thetamax(),
                         closedmin=False, slidermin=stmin)
 stmin.slidermax = stmax
-stmin.on_changed(update_thetamin)
-stmax.on_changed(update_thetamax)
+stmin.on_changed(ax.set_thetamin)
+stmax.on_changed(ax.set_thetamax)
 
 srorigin = mwidgets.Slider(axrorigin, 'rorigin', -5, 5,
                            valinit=ax.get_rorigin(), closedmax=False)
@@ -90,14 +57,14 @@ srmin = mwidgets.Slider(axrmin, 'rmin', -5, 5, valinit=ax.get_rmin(),
 srmax = mwidgets.Slider(axrmax, 'rmax', -5, 5, valinit=ax.get_rmax(),
                         closedmin=False, slidermin=srmin)
 srmin.slidermax = srmax
-srorigin.on_changed(update_rorigin)
-srmin.on_changed(update_rmin)
-srmax.on_changed(update_rmax)
+srorigin.on_changed(ax.set_rorigin)
+srmin.on_changed(ax.set_rmin)
+srmax.on_changed(ax.set_rmax)
 
 rdir = mwidgets.RadioButtons(axdir, ('CCW', 'CW'), active=0)
 rdir.on_clicked(update_dir)
 
 roffs = mwidgets.RadioButtons(axoffs, ('E', 'N', 'W', 'S'), active=0)
-roffs.on_clicked(update_offset)
+roffs.on_clicked(ax.set_theta_zero_location)
 
 plt.show()
